8dabce4acba9583e4f376011565e1aeabacf7d89
Vivendum <vivendum@mail.ru>
2026-04-10 11:24:47 +0500

    Настраивает сборку разметки страниц

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,15 @@ import html_compile from "gulp-nunjucks-render";
 // Setting
 
 import path from "./setting/path.json" with {type: "json"};
+import config_nunjucks from "./setting/config-nunjucks.json" with {type: "json"};
 
 // Page
 
 export const build_html = () => {
   return gulp.src(path.page.take.initial)
     .pipe(html_compile({
-      path: path.page.take.template
+      path: path.page.take.template,
+      envOptions: config_nunjucks //  Разобраться как настроить throwOnUndefined
     }))
     .pipe(gulp.dest(path.page.build.gap));
 };

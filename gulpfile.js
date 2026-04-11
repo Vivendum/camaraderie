6091a885d0af2e50af2e15ee9850541e89b0563c
Vivendum <vivendum@mail.ru>
2026-04-11 14:32:51 +0500

    Автоматизирует линтер для разметки страниц

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ import gulp from "gulp";
 import error_signal from "gulp-plumber";
 import error_ring from "gulp-notify";
 import html_compile from "gulp-nunjucks-render";
+import html_lint from "gulp-htmlhint";
 
 // Setting
 
 import path from "./setting/path.json" with {type: "json"};
 import config_nunjucks from "./setting/config-nunjucks.json" with {type: "json"};
+import config_htmlhint from "./setting/config-htmlhint.json" with {type: "json"};
 
 // Error
 
@@ -37,7 +39,20 @@ export const build_html = () => {
     .pipe(gulp.dest(path.page.build.gap));
 };
 
+// Check
+
+export const lint_html = () => {
+  return gulp.src(path.page.check.gap)
+    .pipe(html_lint(config_htmlhint))
+    .pipe(html_lint.reporter());
+};
+
 // Instruction
+
+export const lint =
+  gulp.parallel(
+    lint_html
+);
 
 export const build =
   gulp.parallel(
